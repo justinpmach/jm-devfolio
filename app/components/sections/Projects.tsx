5fb1c2292295386d5ab15404fbd37d4b4c08c1ec
Justin Mach <justinpmach95@gmail.com>
2024-02-12 17:54:21 -0800

    Updated projects section

--- app/components/sections/Projects.tsx
+++ app/components/sections/Projects.tsx
@@ -8,6 +8,8 @@ interface Project {
   title: string;
   src: string;
   tools: string[];
+  link: string;
+  desc: string;
 }
 
 // Maintained and improved Angular application while simultaneously assisting in a large-scale transition to React and Tailwind
@@ -25,30 +27,40 @@ const projects: Project[] = [
     title: 'AlphaDao',
     src: '/alphadao-web.png',
     tools: ['Next.js', 'Web3.js', 'Tailwind'],
+    link: '',
+    desc: 'Where crypto traders can track the hottest and latest crypto twitter accounts.',
   },
   {
     id: 'dekage',
     title: 'Dekage',
     src: '/dekage-web.png',
     tools: ['Python', 'Django', 'Bootstrap'],
+    link: 'https://www.dekageshop.com/',
+    desc: 'Sticker shop that caters to KPOP and KDrama fans.',
   },
   {
     id: 'the_missing_piece',
     title: 'The Missing Piece',
     src: '/missing-piece.png',
     tools: ['Python', 'Django', 'Bootstrap'],
+    link: 'https://justinmachdev.pythonanywhere.com/',
+    desc: 'Social media app catered to the Autism community, allowing users affected by this disorder to connect.',
   },
   {
     id: 'iron_grind',
     title: 'IronGrind',
     src: '/Iron-Grind-web.png',
     tools: ['HTML', 'CSS', 'Javascript'],
+    link: 'https://vincenttpham.github.io/iron_grind/',
+    desc: 'Fitness club landing page promoting health and wellness through hardwork and discipline.',
   },
   {
     id: 'rocketship',
     title: 'RocketShip',
     src: '/RocketShip-web.png',
     tools: ['C#', 'ASP.NET Core', 'Bootstrap'],
+    link: 'http://18.188.37.161/HowToPlay',
+    desc: 'Rocket Ship is a randomly generated social card game, up to 10 players allowed through different IP addresses. Utilizing the functionality of object oriented programming and version control.',
   },
 ];
 
@@ -74,9 +86,12 @@ export default function Projects() {
           <div className='flex flex-col items-center justify-center w-full p-7'>
             {selected && (
               <>
-                <span className='uppercase text-dark-gray text-xl font-semibold tracking-widest'>
+                <a
+                  href={selected?.link}
+                  className='uppercase text-dark-gray text-xl font-semibold tracking-widest'
+                >
                   {selected?.title}
-                </span>
+                </a>
                 <div className='flex w-full justify-center items-center'>
                   <span className='text-xs mr-2 ml-2 first-of-type:ml-0 last-of-type:mr-0 border-r border-black pr-2 last-of-type:pr-0 last-of-type:border-0'>
                     {selected?.tools?.[0]}
@@ -88,9 +103,12 @@ export default function Projects() {
                     {selected?.tools?.[2]}
                   </span>
                 </div>
+                <div className='flex w-full text-center'></div>
               </>
             )}
-            {/* <div className='mt-8 border border-green-600 w-full'>Text</div> */}
+            <div className='mt-8 flex w-full justify-center items-center'>
+              <p className='text-center text-sm'>{selected?.desc}</p>
+            </div>
           </div>
         </div>
       </div>
